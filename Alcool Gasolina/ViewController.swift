ca3bbe68e7adae44051c4a3bdbbe9c4ca392a3ac
rodolfoizidoro <rodolfoizidoro@gmail.com>
2017-03-02 13:51:09 -0300

    Álcool ou Gasolina
    Alcool ou Gasolina

--- Alcool Gasolina/ViewController.swift
+++ Alcool Gasolina/ViewController.swift
@@ -10,10 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var precoAlcoolTextField: UITextField!
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var precoGasolinaTextField: UITextField!
+    
+    
+    
+    @IBAction func calcularCombustivel(_ sender: Any) {
+        
+        var precoAlcool: Double = 0
+        var precoGasolina: Double = 0
+        var precoResultado: Double = 0
+        
+        //Validações
+        
+        if let resultadoAlcool = precoAlcoolTextField.text{
+            
+            if resultadoAlcool != ""{
+                
+                if let resultadoNumero = Double(resultadoAlcool){
+                    
+                    precoAlcool = resultadoNumero
+                    
+                }
+            
+            }
+        
+        }
+        
+        if let resultadoGasolina = precoGasolinaTextField.text{
+            
+            if resultadoGasolina != ""{
+                
+                if let resultadoNumero = Double(resultadoGasolina){
+                    
+                    precoGasolina = resultadoNumero
+                    
+                }
+                
+            }
+            
+        }
+        
+        //Calculo (precoAlcool / PrecoGasolina) >= 0.7
+        
+        precoResultado = precoAlcool / precoGasolina
+        
+        if precoResultado >= 0.7 {
+            legendaResultado.text = "Melhor utilizar Gasolina!!"
+        }
+        else{
+            legendaResultado.text = "Melhor utilizar Álcool!!"
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
